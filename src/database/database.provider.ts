e56fe749d5cc1d096ea228716d670baf76234b9f
vunguyen <vu@leansoft-inc.com>
2023-09-13 17:33:47 +0700

    refactor: remove .env file

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/test'),
+      mongoose.connect(process.env.MONGODB_URL),
   },
 ];
