c1d39001df5b68a9adb652f8197c8331e70b3c52
vunguyen <vu@leansoft-inc.com>
2023-09-14 11:09:32 +0700

    chore: add commit msg lint and lint stage
    Signed-off-by: vunguyen <vu@leansoft-inc.com>

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+const isCI = process.env.CI;
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional',
+    //'@loopback/monorepo/lib/config-lerna-scopes'
+  ],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [0, 'always'],
+    // Only enforce the rule if CI flag is not set. This is useful for release
+    // commits to skip DCO
+    'signed-off-by': [isCI ? 0 : 2, 'always', 'Signed-off-by:'],
+  },
+};
